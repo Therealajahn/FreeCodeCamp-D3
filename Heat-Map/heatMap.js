4b6ff507401eece4ba048ba9248028ec5a39faa6
A'Jahn Kimble <therealajahn@gmail.com>
2022-05-10 14:31:38 -0500

    heat map cells are positioned and colored, axes look correct

--- Heat-Map/heatMap.js
+++ Heat-Map/heatMap.js
@@ -1,4 +1,4 @@
-let padding = 40;
+let padding = 60;
 let radius = 6;
 let width = 1100;
 let height = 500;
@@ -15,6 +15,20 @@ let yScale;
 let colorScale;
 let tooltip;
 
+const numberToMonth = {
+  1: "January",
+  2: "February",
+  3: "March",
+  4: "April",
+  5: "May",
+  6: "June",
+  7: "July",
+  8: "August",
+  9: "September",
+  10: "October",
+  11: "November",
+  12: "December",
+};
 //prettier-ignore
 
 const canvasOverlay = d3
@@ -29,21 +43,6 @@ const canvas = d3
   .attr("width", `${width}`)
   .attr("height", `${height}`);
 
-let numberToMonth = {
-  0: "December",
-  1: "January",
-  2: "February",
-  3: "March",
-  4: "April",
-  5: "May",
-  6: "June",
-  7: "July",
-  8: "August",
-  9: "September",
-  10: "October",
-  11: "November",
-};
-
 const url =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
 
@@ -54,7 +53,7 @@ fetch(url)
     filterData(json);
     createAxes();
     // createToolTips();
-    // createDots();
+    createRects();
     // createLegend();
   });
 
@@ -65,20 +64,14 @@ function filterData(json) {
 
   data.forEach((d) => {
     yearList.push(d.year);
-    monthList.push(d.month);
+    monthList.push(d.month % 12);
     heatList.push(baseTemperature - d.variance);
   });
 
   xScale = d3.scaleLinear().domain(d3.extent(yearList)).range([0, padWidth]);
-  yScale = d3
-    .scaleLinear()
-    .domain([d3.extent(monthList)])
-    .range([0, padHeight]);
-  colorScale = d3
-    .scaleLinear()
-    .domain(d3.extent(heatList))
-    .range([])
-    .interpolate(d3.interpolateSpectral);
+  yScale = d3.scaleBand().domain(monthList).range([0, padHeight]);
+
+  colorScale = d3.scaleLinear().domain(d3.extent(heatList)).range([0, 1]);
 }
 
 function createAxes() {
@@ -91,10 +84,30 @@ function createAxes() {
     .attr("id", "x-axis")
     .attr("transform", `translate(${padding},${height - padding})`)
     .call(xAxis);
-  let yAxis = d3.axisLeft(yScale);
+  let yAxis = d3.axisLeft(yScale).tickFormat((d, i) => numberToMonth[i + 1]);
   canvas
     .append("g")
     .attr("id", "y-axis")
     .attr("transform", `translate(${padding},${padding})`)
     .call(yAxis);
+}
+
+// function createToolTips(){
+// }
+
+function createRects() {
+  const rect = canvas
+    .selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("class", "cell")
+    .attr("data-year", (d) => d.year)
+    .attr("data-month", (d, i) => monthList[i])
+    .attr("data-temp", (d, i) => heatList[i])
+    .attr("height", `${padHeight / 12}`)
+    .attr("width", `${padWidth / (data.length / 12)}`)
+    .attr("x", (d) => xScale(d.year) + padding)
+    .attr("y", (d, i) => yScale(monthList[i]) + padding)
+    .attr("fill", (d, i) => d3.interpolateSpectral(colorScale(heatList[i])));
 }
